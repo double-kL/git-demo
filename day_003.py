6244d028ee4deab0fd8d060ec617ee4b5d0be8e8
双千流 <1851267870@qq.com>
2026-04-09 16:27:15 +0800

    1

---
+++ day_003.py
@@ -0,0 +1,44 @@
+ 
+"""
+print(0b100)
+print(0o100)
+print(100)
+print(0x100)
+
+print(123.456)
+print(1.23456e2)#科学计数法；
+
+a=45
+b=12
+print(a,b)
+print(a+b)
+print(a*b)
+print(a/b)
+
+a_1=100
+b_1=123.45
+c='hello world'
+d=True
+print(type(a_1))
+print(type(b_1))
+print(type(c))
+print(type(d))
+
+"""
+a=100
+b=123.456
+c='123'
+d='100'
+e='123.456'
+f='hello world'
+g=True
+print(float(a))
+print(int(b))
+print(int (c))
+print(int(c,base=16))
+print(int(d,base=2))
+print(float(e))
+print(bool(f))
+print(int(g))
+print(chr(a))
+print(ord('d'))
---
+++ day_004.py
@@ -0,0 +1,14 @@
+
+"""
+print(8/5)
+print(8//5)
+print(2*3)
+print(2**3)
+print(10)
+print(a:=10)
+print(a)
+f=float(input('请输入华氏温度：'))
+d=(f-32)/1.8
+print('%.1f华氏温度=%.1f摄氏温度'%(f,d))
+
+"""
